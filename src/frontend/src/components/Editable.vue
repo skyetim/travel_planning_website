a05cc3fafaac7a86157681860eb0ed42b9094709
skyetim <wangtian0312@gmail.com>
2019-04-28 07:56:07 +0000

    feat:可更新地址信息，并在地图中展示

--- src/frontend/src/components/Editable.vue
+++ src/frontend/src/components/Editable.vue
@@ -6,20 +6,20 @@
       <thead>
         <tr>
           <td>
-            <input type="checkbox" @click="allSelect" v-model="checked">
+            <input type="checkbox" @click="allSelect" v-model="allChecked">
           </td>
-          <td>行迹</td>
-          <td>时间</td>
+          <td v-for="(header, index) in headers" :key="index" >{{header}}</td>
           <td>操作</td>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(person,index) in travel" :key="index">
           <td>
-            <input type="checkbox" v-model="selected" v-bind:value="person.time">
+            <input type="checkbox" v-model="selected" v-bind:value="index">
           </td>
-          <td @click="edit(index)" contenteditable="true">{{person.time}}</td>
-          <td @click="edit(index)" contenteditable="true">{{person.loc}}</td>
+          <td v-for="(value, key) in person" :key="key">
+            <input type="text" v-model="travel[index][key]">
+          </td>
           <td>
             <button @click="del(index)" class="btn btn-danger btn-sm">删除</button>
             <button @click="update(index)" class="btn btn-info btn-sm">编辑</button>
@@ -27,41 +27,7 @@
         </tr>
       </tbody>
     </table>
-
-    <fieldset v-show="seen">
-      <legend>新增行迹</legend>
-      <div class>
-        <p>
-          <label>地点：</label>
-          <input type="text" v-model="newTravel.loc">
-        </p>
-        <p>
-          <label>时间：</label>
-          <input type="number" v-model="newTravel.time">
-        </p>
-        <p>
-          <button class="btn btn-success btn-sm submit" @click="add">提交</button>
-        </p>
-      </div>
-    </fieldset>
-    <!-- 编辑界面 -->
-    <fieldset v-show="editSeen">
-      <legend>编辑行迹</legend>
-
-      <div class>
-        <p>
-          <label>地点：</label>
-          <input type="text" v-model="editTravel.loc" >
-        </p>
-        <p>
-          <label>时间：</label>
-          <input type="number" v-model="editTravel.time" >
-        </p>
-        <p>
-          <button class="btn btn-success btn-sm submit" @click="editSubmit">提交</button>
-        </p>
-      </div>
-    </fieldset>
+    <div>{{this.travel}}</div>
   </div>
 </template>
 
@@ -72,46 +38,33 @@ export default {
   name: "editable",
   data(){
     return {
-              newTravel: {
-                loc: "",
-                sage: ""
-              },
-              seen: false,
-              editSeen: false,
-              checked: false,
+              allChecked: false,
               selected: [],
-              editTravel: {
-                loc: "",
-                sage: ""
-              }
             }
   }, 
   props: {
-    travel: Array
+    travel: Array,
+    headers: Array,
   },
   methods: {
     // 添加
     addBox: function() {
-      this.seen = this.seen == false ? true : false;
+      var newLine = new Array(this.headers);
+      for(var i=0; i<this.headers.length; ++i){
+        newLine[i] = "";
+      }
+      this.travel.push(newLine);
     },
     //删除
     del: function(index) {
       this.travel.splice(index, 1);
-    },
-    //提交
-    add: function() {
-      //插入到travel中
-      this.travel.push(this.newTravel);
-
-      this.newTravel = {};
-      this.seen = false;
     },
     //全选
     allSelect: function() {
       if (this.selected.length != this.travel.length) {
         this.selected = [];
         for (var i = 0; i < this.travel.length; i++) {
-          this.selected.push(this.travel[i].time);
+          this.selected.push(i);
         }
       } else {
         this.selected = [];
@@ -119,30 +72,20 @@ export default {
     },
     //批量删除
     delAll: function() {
-      for (var j = 0; j < this.selected.length; j++) {
-        for (var i = 0; i < this.travel.length; i++) {
-          if (this.selected[j] == this.travel[i].time) {
-            this.travel.splice(i, 1);
-          }
-        }
+      this.selected = this.selected.sort();
+      for (var j = this.selected.length - 1; j > -1; j--) {
+          this.travel.splice(this.selected[j], 1);
       }
-    },
-    //编辑
-    update: function(index) {
-      this.editSeen = true;
-      this.editTravel = this.travel[index];
-    },
-    //编辑后提交
-    editSubmit: function() {
-      this.editSeen = false;
+      this.selected = [];
+      this.allChecked = false;
     }
   },
   watch: {
     selected: function() {
       if (this.selected.length == this.travel.length) {
-        this.checked = true;
+        this.allChecked = true;
       } else {
-        this.checked = false;
+        this.allChecked = false;
       }
     }
   }
@@ -166,10 +109,6 @@ table tr td {
 }
 /*全删*/
 .delAll {
-  margin-left: 10px;
-}
-/*新增*/
-fieldset {
   margin-left: 10px;
 }
 </style>
--- src/frontend/src/views/Maps.vue
+++ src/frontend/src/views/Maps.vue
@@ -2,8 +2,9 @@
   <div>
     <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
       <div class="row">
-          <editable :travel="travel"></editable>
+          <editable :travel="travel" :headers="headers"></editable>
       </div>
+    <button class="btn btn-primary btn-sm addBox" @click="save()">保存</button>
     </base-header>
 
     <div class="container-fluid mt--7">
@@ -22,53 +23,58 @@
 <script>
 import L from 'leaflet';
 
-var travel=[
-    { loc: "张三", time: 18 },
-    { loc: "张三", time: 19 },
-    { loc: "张三", time: 20 },
-    { loc: "张三", time: 21 }
+var headers = ["行迹", "时间", "经度", "纬度"];
+var travel = [
+    ["北京", "2019", "116.30", "39.95"],
+    ["上海", "2019", "121.47", "31.23"]
 ]
-
 export default {
     data(){
       return {
-        travel: travel
+        travel: travel,
+        headers: headers,
+        mymap: null
       }
     },
     mounted(){
-        var mymap = L.map('map-canvas').setView([39.9877, 116.3075], 6);
-        // tile
-        L.tileLayer(
-            'https://api.mapbox.com/styles/v1/oymisaki/cjv0cnpx43bzn1gqjofoc98q8/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoib3ltaXNha2kiLCJhIjoiY2p0ZWEwZDRlMWcwcTQzbW9xcWd5MnpxbyJ9.Ri_EK6iwsLzeH-ZgxJg0ig', 
-            {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-                subdomains: 'abcd',
-                maxZoom: 19
-            }).addTo(mymap);
-        // set my marker
+      this.mymap =  L.map('map-canvas').setView([39.9877, 116.3075], 4);
 
-        L.marker([39.9877, 116.3075]).addTo(mymap)
-        .bindPopup("<b>你好！</b><br />我在北京大学第三教学楼！").openPopup();
-        var popup = L.popup();
-        var lastLoc = [39.9877, 116.3075];
-        function onMapClick(e) {
-            // popup
-            popup
-                .setLatLng(e.latlng)
-                .setContent("You arrived here: " + e.latlng.toString())
-                .openOn(mymap);
-
-            // marker
-            L.marker(e.latlng).addTo(mymap);
-            var latlngs = [
+      // tile
+      L.tileLayer(
+        'https://api.mapbox.com/styles/v1/oymisaki/cjv0cnpx43bzn1gqjofoc98q8/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoib3ltaXNha2kiLCJhIjoiY2p0ZWEwZDRlMWcwcTQzbW9xcWd5MnpxbyJ9.Ri_EK6iwsLzeH-ZgxJg0ig', 
+        {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            subdomains: 'abcd',
+            maxZoom: 19
+        }).addTo(this.mymap);
+      var lastLoc = [parseFloat(travel[0][3]), parseFloat(travel[0][2])];
+      travel.forEach(element => {
+        var nowLoc = [parseFloat(element[3]), parseFloat(element[2])];
+        L.marker(nowLoc).addTo(this.mymap).bindPopup("你在"+element[1]+"来过这里").openPopup();
+        var latlngs = [
+              lastLoc,
+              nowLoc
+          ];
+        L.polyline(latlngs, { color: '#1e90ff' }).addTo(this.mymap);
+        lastLoc = nowLoc;
+      });
+    },
+    methods:{
+      // 保存
+      save: function(){
+        // 提交表单到数据库
+        var lastLoc = [parseFloat(travel[0][3]), parseFloat(travel[0][2])];
+        travel.forEach(element => {
+          var nowLoc = [parseFloat(element[3]), parseFloat(element[2])];
+          L.marker(nowLoc).addTo(this.mymap).bindPopup("你在"+element[1]+"来过这里").openPopup();
+          var latlngs = [
                 lastLoc,
-                e.latlng
-            ]
-            // line
-            L.polyline(latlngs, { color: '#1e90ff' }).addTo(mymap);
-            lastLoc = e.latlng;
-        }
-        mymap.on('click', onMapClick);
+                nowLoc
+            ];
+          L.polyline(latlngs, { color: '#1e90ff' }).addTo(this.mymap);
+          lastLoc = nowLoc;
+        });
+      }
     }
 }
 </script>
