f210bc9fed890c1aef7ecd65b6e0e64e98812869
skyetim <wangtian0312@gmail.com>
2019-05-05 14:41:52 +0000

    feat: redirect to login if not logged in

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -7,7 +7,30 @@ import ArgonDashboard from './plugins/argon-dashboard'
 Vue.config.productionTip = false
 
 Vue.use(ArgonDashboard)
+
+function getCookie(name) {
+  var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+  if (arr = document.cookie.match(reg))
+    return (arr[2]);
+  else
+    return null;
+}
+Vue.prototype.getCookie = getCookie;
+
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App), 
+  created() {
+    this.checkLogin();
+  }, 
+  methods: {
+    checkLogin(){
+      if (!this.getCookie('sessionid') && this.$route.name!='register') {
+        this.$router.push('/login');
+      } 
+    }
+  }, 
+  watch:{
+    '$route': 'checkLogin'
+  }
 }).$mount('#app')
