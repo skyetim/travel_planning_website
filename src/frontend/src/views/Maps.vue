eafca98269f4e125d42902225c2b1c92bb0f78b6
oymisaki <1500015877@pku.edu.cn>
2019-05-12 09:30:19 +0000

    feat: 地图marker的颜色可以改变

--- src/frontend/src/views/Maps.vue
+++ src/frontend/src/views/Maps.vue
@@ -1,15 +1,10 @@
 <template>
   <div>
-    <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
-    </base-header>
-
     <div class="container-fluid mt--7">
       <div class="row">
         <div class="col">
           <div class="card shadow border-0">
-            <div id="map-canvas" class="map-canvas" style="height: 600px;">
-                
-            </div>
+            <div id="map-canvas" class="map-canvas" style="height: 600px;"></div>
           </div>
         </div>
       </div>
@@ -17,108 +12,121 @@
   </div>
 </template>
 <script>
-import L from 'leaflet';
+import L from "leaflet";
 
-var travel = [
+var color_picker = ["#f5365c", "#172b4d", "#fb6340", "#11cdef", "#2dce89"];
+
+var travelGroup = [
   {
     name: "balabala之旅",
-    travel: [{location: "广州", coordinate: [23.16667, 113.23333]},
-            {location: "深圳", coordinate: [22.61667, 114.06667]},
-            {location: "北京", coordinate: [39.95, 116.3]},
-            {location: "上海", coordinate: [31.23, 121.47]},
-            {location: "南京", coordinate: [32.05, 118.78333]}],
+    travel: [
+      { location: "广州", coordinate: [23.16667, 113.23333] },
+      { location: "深圳", coordinate: [22.61667, 114.06667] }
+    ],
     dates: { start: "2019-05-01", end: "2019-07-19" },
-    status: 2,
+    status: 2
   },
   {
     name: "开心之旅",
-    travel: [{location: "广州", coordinate: [23.16667, 113.23333]},
-            {location: "深圳", coordinate: [22.61667, 114.06667]},
-            {location: "北京", coordinate: [39.95, 116.3]},
-            {location: "上海", coordinate: [31.23, 121.47]},
-            {location: "南京", coordinate: [32.05, 118.78333]}],
+    travel: [
+      { location: "北京", coordinate: [39.95, 116.3] },
+      { location: "上海", coordinate: [31.23, 121.47] }
+    ],
     dates: { start: "2019-07-17", end: "2019-07-19" },
     status: 0
   },
   {
     name: "LOL之旅",
-    travel: [{location: "广州", coordinate: [23.16667, 113.23333]},
-            {location: "深圳", coordinate: [22.61667, 114.06667]},
-            {location: "北京", coordinate: [39.95, 116.3]},
-            {location: "上海", coordinate: [31.23, 121.47]},
-            {location: "南京", coordinate: [32.05, 118.78333]}],
+    travel: [{ location: "南京", coordinate: [32.05, 118.78333] }],
     dates: { start: "2018-07-17", end: "2018-07-19" },
     status: 3
-  },
-  {
-    name: "有意思的行程",
-    travel: [{location: "广州", coordinate: [23.16667, 113.23333]},
-            {location: "深圳", coordinate: [22.61667, 114.06667]},
-            {location: "北京", coordinate: [39.95, 116.3]},
-            {location: "上海", coordinate: [31.23, 121.47]},
-            {location: "南京", coordinate: [32.05, 118.78333]}],
-    dates: { start: "2018-07-17", end: "2018-07-19" },
-    status: 1
-  },
-  {
-    name: "Go to Hell!",
-    travel: [{location: "广州", coordinate: [23.16667, 113.23333]},
-            {location: "深圳", coordinate: [22.61667, 114.06667]},
-            {location: "北京", coordinate: [39.95, 116.3]},
-            {location: "上海", coordinate: [31.23, 121.47]},
-            {location: "南京", coordinate: [32.05, 118.78333]}],
-    dates: { start: "2018-07-17", end: "2018-07-19" },
-    status: 0  
   }
 ];
-      
-function mountMap(map, travel){
-  var markers = [];
 
-  travel.forEach(element => {
-    var nowLoc = element.coordinate;
-    var marker = L.marker(nowLoc)
-                  .bindPopup("你在"+element.dates.range.replace('to', '至')+"来过这里")
-                  .addTo(map);
-    markers.push(marker);
+function mountMap(map, travelGroup) {
+  var markersGroup = [];
+
+  travelGroup.forEach(travel => {
+    var markers = [];
+    var myCustomColour = color_picker[travel.status];
+    const markerHtmlStyles = `
+      background-color: ${myCustomColour};
+      opacity: 0.8;
+      width: 2.5rem;
+      height: 2.5rem;
+      display: block;
+      left: -1.5rem;
+      top: -1.5rem;
+      position: relative;
+      border-radius: 3rem 3rem 0;
+      transform: rotate(45deg);
+      border: 1px solid #FFFFFF;`;
+    const myicon = L.divIcon({
+      className: "my-custom-pin",
+      iconAnchor: [0, 24],
+      labelAnchor: [-6, 0],
+      popupAnchor: [0, -36],
+      html: `<span style="${markerHtmlStyles}" />`
+    });
+    travel.travel.forEach(element => {
+      var marker = L.marker(element.coordinate, {
+        icon: myicon
+      })
+        .bindPopup(
+          "你在" +
+            travel.dates.start +
+            "至" +
+            travel.dates.end +
+            "来过" +
+            element.location
+        )
+        .addTo(map);
+      markers.push(marker);
+    });
+    markersGroup.push(markers);
   });
 
-  return markers;
+  return markersGroup;
 }
 
 export default {
-    data(){
-      return {
-        travel: travel,
-        map: null,
-        markers: []
-      }
-    },
-    mounted(){
-      this.map =  L.map('map-canvas').setView([39.9877, 116.3075], 4);
+  data() {
+    return {
+      travelGroup: travelGroup,
+      map: null,
+      markersGroup: []
+    };
+  },
+  mounted() {
+    this.map = L.map("map-canvas").setView([39.9877, 116.3075], 4);
 
-      // tile
-      L.tileLayer(
-        'https://api.mapbox.com/styles/v1/oymisaki/cjv1qcq5g0rvu1fjmh365xxs1/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoib3ltaXNha2kiLCJhIjoiY2p0ZWEwZDRlMWcwcTQzbW9xcWd5MnpxbyJ9.Ri_EK6iwsLzeH-ZgxJg0ig',
+    // tile
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/oymisaki/cjv1qcq5g0rvu1fjmh365xxs1/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoib3ltaXNha2kiLCJhIjoiY2p0ZWEwZDRlMWcwcTQzbW9xcWd5MnpxbyJ9.Ri_EK6iwsLzeH-ZgxJg0ig",
 
-        {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-            subdomains: 'abcd',
-            maxZoom: 21
-        }).addTo(this.map);
-      this.markers = mountMap(this.map, this.travel);
-    },
-    methods:{
-      // 保存
-      save: function(){
-        // 提交表单到数据库
-        for(var i = 0; i < this.markers.length; i++){
-            this.map.removeLayer(this.markers[i]);
-        }
-        this.markers = mountMap(this.map, this.travel);
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: "abcd",
+        maxZoom: 21
       }
+    ).addTo(this.map);
+    this.markersGroup = mountMap(this.map, this.travelGroup);
+  },
+  methods: {
+    // 保存
+    save: function() {
+      // 提交表单到数据库
+      for (var i = 0; i < this.markersGroup.length; i++) {
+        this.map.removeLayer(this.markers[i]);
+      }
+      this.markersGroup = mountMap(this.map, this.travel);
     }
-}
+  }
+};
 </script>
 <style>
+.my-div-icon {
+  color: "#172b4d";
+}
 </style>
