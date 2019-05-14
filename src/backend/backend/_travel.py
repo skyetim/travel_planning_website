f013c2274f95293922d12bdb62299b5f3a7d2efe
MagShadow <commander.yang.v5@gmail.com>
2019-05-15 01:11:18 +0800

    feat: 开始写后端类，定义格式

---
+++ src/backend/backend/_travel.py
@@ -0,0 +1,13 @@
+from _user import User, FriendInfo
+
+class Travel():
+    def __init__(self):
+        pass
+
+class TravelInfo():
+    def __init__(self):
+        pass
+
+class TravelGroup():
+    def __init__(self):
+        pass
---
+++ src/backend/backend/_user.py
@@ -0,0 +1,22 @@
+from _travel import Travel, TravelGroup
+# import _travel
+
+
+class User():
+    def __init__(self):
+        pass
+
+
+class UserInfoBase():
+    def __init__(self):
+        pass
+
+
+class UserInfo(UserInfoBase):
+    def __init__(self):
+        super()
+
+
+class FriendInfo(UserInfoBase):
+    def __init__(self):
+        super()
