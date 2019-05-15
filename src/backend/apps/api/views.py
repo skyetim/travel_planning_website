45f0baa5c97bc0d7357f1f1ab2c5c2fa76928b3d
XuehaiPan <XuehaiPan@pku.edu.cn>
2019-05-15 21:44:59 +0800

    refactor: refactor api with decorator

--- src/backend/apps/api/views.py
+++ src/backend/apps/api/views.py
@@ -8,84 +8,102 @@ import apps.db.User.models as db_user
 from apps.api.modules.exceptions import *
 
 
-__all__ = ['login', 'register', 'reset_password', 'address_to_city']
+__all__ = []
+__all__.extend(['login', 'register', 'reset_password'])
+__all__.extend(['address_to_city'])
 
 logged_in_users = {}
 
 
-def check_authorization(request):
-    pass
+def check_authentication(func):
+    def wrapper(request):
+        # check_authentication
+
+        return func(request=request)
+
+    return wrapper
 
 
-def check_token(request):
-    try:
-        user_session = db_user.UserSession.objects.get(user_id=request.GET.get('user_id'),
-                                                       session_id=request.GET.get('session_id'))
-    except ObjectDoesNotExist:
-        db_user.UserSession.objects.filter(user_id=request.GET.get('user_id')).delete()
-        raise UserAuthorizationException('Authorization error.')
+def check_token(func):
+    def wrapper(request):
+        try:
+            user_session = db_user.UserSession.objects.get(user_id=request.GET.get('user_id'),
+                                                           session_id=request.GET.get('session_id'))
+        except ObjectDoesNotExist:
+            db_user.UserSession.objects.filter(user_id=request.GET.get('user_id')).delete()
+            raise UserAuthorizationException('Authorization error.')
+        else:
+            return func(request=request)
+
+    return wrapper
+
+
+def pack_response(func):
+    def wrapper(request):
+        try:
+            response = func(request=request)
+        except BackendBaseException as e:
+            response = {'status': e.CODE, 'error_message': str(e)}
+        return JsonResponse(response)
+
+    return wrapper
 
 
 # Create your views here.
 @require_http_methods(["GET"])
+@pack_response
+@check_authentication
 def login(request):
-    check_authorization(request=request)
+    user = mod_user.User(email=request.GET.get('email'),
+                         pswd_hash=request.GET.get('pswd_hash'))
+    logged_in_users[user.get_user_id()] = user
 
-    response = {}
-    try:
-        user = mod_user.User(email=request.GET.get('email'),
-                             pswd_hash=request.GET.get('pswd_hash'))
-        response['user_id'] = user.get_user_id()
-        response['session_id'] = user.get_session_id()
-        response['status'] = 0
-        logged_in_users[user.get_user_id()] = user
-    except BackendBaseException as e:
-        response['status'] = e.CODE
-        response['error_message'] = str(e)
-
-    return JsonResponse(response)
+    response = {
+        'user_id': user.get_user_id(),
+        'session_id': user.get_session_id(),
+        'status': 0
+    }
+    return response
 
 
 @require_http_methods(["GET"])
+@pack_response
+@check_authentication
 def register(request):
-    check_authorization(request=request)
+    user = mod_user.User.new_user(email=request.GET.get('email'),
+                                  pswd_hash=request.GET.get('pswd_hash'),
+                                  user_name=request.GET.get('user_name'),
+                                  gender=request.GET.get('gender'),
+                                  resident_city_id=int(request.GET.get('resident_city_id')))
 
-    response = {}
-    try:
-        user = mod_user.User.new_user(email=request.GET.get('email'),
-                                      pswd_hash=request.GET.get('pswd_hash'),
-                                      user_name=request.GET.get('user_name'),
-                                      gender=request.GET.get('gender'),
-                                      resident_city_id=int(request.GET.get('resident_city_id')))
-        response['user_id'] = user.get_user_id()
-        response['status'] = 0
-    except BackendBaseException as e:
-        response['status'] = e.CODE
-        response['error_message'] = str(e)
-
-    return JsonResponse(response)
+    response = {
+        'user_id': user.get_user_id(),
+        'status': 0
+    }
+    return response
 
 
 @require_http_methods(["GET"])
+@pack_response
+@check_token
+@check_authentication
 def reset_password(request):
-    check_authorization(request=request)
-    check_token(request=request)
+    user = logged_in_users[request.GET.get('user_id')]
+    user.reset_password(old_pswd_hash=request.GET.get('old_pswd_hash'),
+                        new_pswd_hash=request.GET.get('new_pswd_hash'))
 
-    response = {}
-    try:
-        user = logged_in_users[request.GET.get('user_id')]
-        user.reset_password(old_pswd_hash=request.GET.get('old_pswd_hash'),
-                            new_pswd_hash=request.GET.get('new_pswd_hash'))
-    except BackendBaseException as e:
-        response['status'] = e.CODE
-        response['error_message'] = str(e)
-
-    return JsonResponse(response)
+    response = {
+        'status': 0
+    }
+    return response
 
 
 @require_http_methods(["GET"])
+@pack_response
+@check_authentication
 def address_to_city(request):
-    city = mod_city.address_to_city_instance(address=request.GET.get('address'))
+    city = mod_city.get_city_instance(address=request.GET.get('address'))
+
     response = {
         'city_id': city.city_id,
         'country_name': city.country_name,
@@ -93,4 +111,4 @@ def address_to_city(request):
         'city_name': city.city_name,
         'status': 0
     }
-    return JsonResponse(response)
+    return response
