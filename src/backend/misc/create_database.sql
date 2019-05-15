f215163c0c42e0519414fce8588d2d39d7d84862
XuehaiPan <XuehaiPan@pku.edu.cn>
2019-05-15 11:10:33 +0800

    refactor: add namespace to table name

--- src/backend/misc/create_database.sql
+++ src/backend/misc/create_database.sql
@@ -4,7 +4,7 @@ create schema if not exists SoftwareEngineeringProject;
 use SoftwareEngineeringProject;
 
 -- Create tables
-create table if not exists Cities
+create table if not exists Cities_Cities
 (
     city_id       int          not null auto_increment,
     country_name  nvarchar(20) not null,
@@ -12,13 +12,13 @@ create table if not exists Cities
     city_name     nvarchar(20) not null,
     latitude      float        not null,
     longitude     float        not null,
-    
+
     -- clustered index
     constraint PK_Cities
         primary key (city_id, country_name, province_name, city_name)
 );
 
-create table if not exists Users
+create table if not exists Users_Users
 (
     user_id          int                 not null auto_increment,
     user_name        nvarchar(20)        not null,
@@ -26,11 +26,11 @@ create table if not exists Users
     email            varchar(25)         not null unique,
     gender           char(1) default 'U' not null,
     resident_city_id int                 not null,
-    
+
     -- clustered index
     constraint PK_Users
         primary key (user_id, email),
-    
+
     constraint FK_Users_Cities
         foreign key (resident_city_id)
             references Cities (city_id),
@@ -44,16 +44,16 @@ create table if not exists Users
             ))
 );
 
-create table if not exists FriendRelations
+create table if not exists Users_FriendRelations
 (
     user_id          int not null,
     friend_user_id   int not null,
     friend_user_note nvarchar(20),
-    
+
     -- clustered index
     constraint PK_FriendRelations
         primary key (user_id, friend_user_id),
-    
+
     constraint FK_FriendRelations_User
         foreign key (user_id)
             references Users (user_id),
@@ -62,25 +62,25 @@ create table if not exists FriendRelations
             references Users (user_id)
 );
 
-create table if not exists TravelGroups
+create table if not exists Travels_TravelGroups
 (
     travel_groups_id   int           not null auto_increment,
     travel_groups_note nvarchar(140) not null,
-    
+
     -- clustered index
     constraint PK_TravelGroups
         primary key (travel_groups_id)
 );
 
-create table if not exists TravelGroupOwnership
+create table if not exists Travels_TravelGroupOwnership
 (
     user_id          int not null,
     travel_groups_id int not null,
-    
+
     -- clustered index
     constraint PK_TravelGroupOwnership
         primary key (user_id, travel_groups_id),
-    
+
     constraint FK_TravelGroupOwnership_Users
         foreign key (user_id)
             references Users (user_id),
@@ -89,7 +89,7 @@ create table if not exists TravelGroupOwnership
             references TravelGroups (travel_groups_id)
 );
 
-create table if not exists Travels
+create table if not exists Travels_Travels
 (
     travel_id   int                 not null auto_increment,
     date_start  date                not null,
@@ -97,11 +97,11 @@ create table if not exists Travels
     city_id     int                 not null,
     visibility  char(1) default 'F' not null,
     travel_note nvarchar(140)       not null,
-    
+
     -- clustered index
     constraint PK_Travels
         primary key (travel_id),
-    
+
     constraint FK_Travels_Cities
         foreign key (city_id)
             references Cities (city_id),
@@ -112,20 +112,20 @@ create table if not exists Travels
             ))
 );
 
-alter table Travels
+alter table Travels_Travels
     add index (date_start, date_end);
-alter table Travels
+alter table Travels_Travels
     add index (city_id);
 
-create table if not exists TravelGrouping
+create table if not exists Travels_TravelGrouping
 (
     travel_id        int not null,
     travel_groups_id int not null,
-    
+
     -- clustered index
     constraint PK_TravelGrouping
         primary key (travel_id, travel_groups_id),
-    
+
     constraint FK_TravelGrouping_Travels
         foreign key (travel_id)
             references Travels (travel_id),
@@ -134,15 +134,15 @@ create table if not exists TravelGrouping
             references TravelGroups (travel_groups_id)
 );
 
-create table if not exists TravelAssociation
+create table if not exists Travels_TravelAssociation
 (
     travel_id       int not null,
     company_user_id int not null,
-    
+
     -- clustered index
     constraint PK_TravelAssociation
         primary key (travel_id),
-    
+
     constraint FK_TravelAssociation_Travels
         foreign key (travel_id)
             references Travels (travel_id),
@@ -151,18 +151,18 @@ create table if not exists TravelAssociation
             references Users (user_id)
 );
 
-create table if not exists FriendRequests
+create table if not exists Messages_FriendRequests
 (
     msg_id         int                 not null auto_increment,
     user_id        int                 not null,
     friend_user_id int                 not null,
     msg_type       char(1) default 'A' not null,
     msg_content    nvarchar(140)       not null,
-    
+
     -- clustered index
     constraint PK_FriendRequests
         primary key (msg_id, user_id),
-    
+
     constraint FK_FriendRequests_User
         foreign key (user_id)
             references Users (user_id),
@@ -175,7 +175,7 @@ create table if not exists FriendRequests
             ))
 );
 
-create table if not exists TravelAssociation
+create table if not exists Messages_TravelAssociation
 (
     msg_id         int           not null auto_increment,
     user_id        int           not null,
@@ -183,11 +183,11 @@ create table if not exists TravelAssociation
     travel_id      int           not null,
     msg_type       char(1)       not null,
     msg_content    nvarchar(140) not null,
-    
+
     -- clustered index
     constraint PK_TravelAssociation
         primary key (msg_id, user_id),
-    
+
     constraint FK_TravelAssociation_User
         foreign key (user_id)
             references Users (user_id),
