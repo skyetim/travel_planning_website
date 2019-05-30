a14963006bfcf086683218473a2e05e98f001e18
skyetim <wangtian0312@gmail.com>
2019-05-30 15:04:10 +0000

    chore: add new status

--- src/frontend/src/status.js
+++ src/frontend/src/status.js
@@ -2,5 +2,7 @@ export const status = {
         'normal': 0, 
         'wrong_password': 2, 
         'user_does_not_exist': 3, 
-        'user_already_exists': 4
+        'user_already_exists': 4,
+        'user_anthorization_error': 5,
+        'user_session_timeout': 23
     }
