d57390c8b02a5e7d634fc6f69763d273c58df68f
skyetim <wangtian0312@gmail.com>
2019-05-31 14:10:13 +0000

    chore: minorly change display

--- src/frontend/src/views/UserProfile.vue
+++ src/frontend/src/views/UserProfile.vue
@@ -8,7 +8,7 @@
             <div class="container-fluid d-flex align-items-center">
                 <!-- <div class="row"> -->
                     <div class="col-lg-7 col-md-10">
-                        <h1 class="display-2 text-white">Hi {{model.first_name}}, </h1>
+                        <h1 class="display-2 text-white">Hi {{this.$session.get('user_name')}}, </h1>
                         <p class="text-white mt-0 mb-5">你可以在这里修改自己的个人信息</p>
                         <!-- <a href="#!" class="btn btn-info">Edit profile</a> -->
                     </div>
