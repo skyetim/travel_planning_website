8fe619166488b67fce4bd785d09a00bc8c7a2d81
oymisaki <1500015877@pku.edu.cn>
2019-06-03 07:38:42 +0000

    feat: 可以实时相应颜色和其他更改

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -8,10 +8,9 @@ import 'leaflet/dist/leaflet.css';
 import VueSession from 'vue-session';
 import VueResource from 'vue-resource';
 import md5 from 'js-md5';
-import {status} from './status';
-import {backend} from './backend_config';
+import { status } from './status';
+import { backend } from './backend_config';
 import Vuelidate from 'vuelidate';
-
 
 Vue.use(VueSession);
 Vue.use(VueResource);
@@ -19,8 +18,8 @@ Vue.use(Vuelidate);
 Vue.prototype.$md5 = md5;
 Vue.prototype.$status = status;
 Vue.prototype.$backend = backend;
-Vue.prototype.compare = function(id) {
-  return function(obj1, obj2) {
+Vue.prototype.compare = function (id) {
+  return function (obj1, obj2) {
     if (obj1[id] < obj2[id]) {
       return -1;
     } else if (obj1[id] > obj2[id]) {
@@ -30,6 +29,27 @@ Vue.prototype.compare = function(id) {
     }
   };
 }
+Vue.prototype.newTravelGroup = function () {
+  var travelGroupProto = {
+    name: "",
+    travel_group_id: null,
+    travel: [],
+    dates: { start: "", end: "" },
+    color:{hex: "#ffffff", a:0.8}
+  };
+  return travelGroupProto;
+}
+Vue.prototype.copy = function(obj) {
+  let newObj = JSON.parse(JSON.stringify(obj));
+  return newObj;
+}
+Vue.prototype.indexOf =  function(arr, el) {
+  for (var i = 0; i < arr.length; ++i) {
+    if (arr[i] == el) {
+      return i;
+    }
+  }
+},
 Vue.config.silent = true;
 
 Vue.http.options.emulateJSON = true;
@@ -48,20 +68,20 @@ Vue.use(ArgonDashboard)
 
 new Vue({
   router,
-  render: h => h(App), 
+  render: h => h(App),
   created() {
     this.checkLogin();
-  }, 
+  },
   methods: {
-    checkLogin(){
-      if (!this.$session.exists() && this.$route.name!='register') { // 如果没有login且不在register页自动跳转
+    checkLogin() {
+      if (!this.$session.exists() && this.$route.name != 'register') { // 如果没有login且不在register页自动跳转
         this.$router.push('/login');
-      } else if (this.$session.exists() && (this.$route.name=='login' || this.$route.name=='register')){ // 如果已经login进入login页自动跳转
+      } else if (this.$session.exists() && (this.$route.name == 'login' || this.$route.name == 'register')) { // 如果已经login进入login页自动跳转
         this.$router.push('/');
       }
     }
-  }, 
-  watch:{
+  },
+  watch: {
     '$route': 'checkLogin'
   }
 }).$mount('#app')
--- src/frontend/src/views/Maps.vue
+++ src/frontend/src/views/Maps.vue
@@ -1,30 +1,78 @@
 <template>
   <div>
-    <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
-      <travel-stats :travelGroup="travel_group_list"/>
-    </base-header>
+    <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8"></base-header>
 
     <div class="container-fluid mt--7">
       <div class="row">
         <div class="col">
           <div class="card shadow border-0">
             <div class="color-picker">
-              <base-dropdown v-for="(travel_group, index) in travel_group_list" :key="index">
-                <button
-                  slot="title"
-                  class="btn dropdown-toggle button-text"
-                  :style="{backgroundColor:travel_group.color.hex, opacity:travel_group.color.a}"
-                >{{travel_group.name}}</button>
-                <div>
-                  <swatches v-model="travel_group.color"></swatches>
-                </div>
-              </base-dropdown>
+              <button
+                v-for="(travel_group, index) in travel_group_list"
+                :key="index"
+                @click="editTravel(travel_group_list[index])"
+                slot="title"
+                class="btn dropdown-toggle button-text"
+                :style="{backgroundColor:travel_group_list[index].color.hex, opacity:travel_group_list[index].color.a}"
+              >{{travel_group_list[index].name}}</button>
+              <base-button
+                type="primary"
+                @click="edit.addMode = true;edit.modal = true;editRow=newTravelGroup();"
+              >添加新的行程</base-button>
             </div>
             <div id="map-canvas" class="map-canvas" style="height: 600px;"></div>
           </div>
         </div>
       </div>
     </div>
+
+    <modal :show.sync="edit.modal">
+      <template slot="header">
+        <h5 class="modal-title" id="exampleModalLabel">{{edit.addMode? "新建行迹" : "修改行迹"}}</h5>
+      </template>
+      <div>
+        <small class="text-muted text-center">行迹名</small>
+        <br>
+        <div class="row">
+          <div class="col-11 inline-div">
+            <base-input placeholder="行迹名" v-model="editRow.name" ref="travel_group_name"></base-input>
+          </div>
+          <div class="inline-div">
+            <i
+              :class="['ni', edit.collapsed ? 'ni-bold-down': 'ni-bold-up', 'icon-expand']"
+              @click="edit.collapsed=!edit.collapsed;"
+            ></i>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-11">
+            <div :class="[edit.collapsed?'collapse': 'expand']">
+              <draggablelist :travel="editRow.travel" :gid="editRow.travel_group_id"></draggablelist>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-11">
+            <base-dropdown>
+              <button
+                slot="title"
+                class="btn dropdown-toggle button-text"
+                :style="{backgroundColor:editRow.color.hex, opacity:editRow.color.a}"
+              >行迹颜色</button>
+              <div>
+                <swatches v-model="editRow.color"></swatches>
+              </div>
+            </base-dropdown>
+          </div>
+        </div>
+      </div>
+      <template slot="footer">
+        <base-button
+          type="primary"
+          @click="edit.addMode?add_travel_group(editRow):set_travel_group(editRow, row);edit.modal = false;"
+        >Save changes</base-button>
+      </template>
+    </modal>
   </div>
 </template>
 <script>
@@ -83,12 +131,25 @@ function mountMap(map, travel_group_list) {
   return markersGroup;
 }
 
+var edit = {
+  addMode: false,
+  modal: false,
+  collapsed: true
+};
+
 export default {
   components: {
     swatches
   },
   data() {
     return {
+      edit: {
+        addMode: false,
+        modal: false,
+        collapsed: true
+      },
+      editRow: this.newTravelGroup(),
+      editIndex: null,
       travel_group_list: [],
       map: null,
       markersGroup: []
@@ -170,17 +231,18 @@ export default {
   watch: {
     travel_group_list: {
       handler: function() {
-        try {
-          this.changeColor();
-        } catch (err) {
-          console.log(err);
-        }
+        // try {
+        //   this.reMount();
+        // } catch (err) {
+        //   console.log(err);
+        // }
+        this.reMount();
       },
       deep: true
     }
   },
   methods: {
-    changeColor: function() {
+    reMount: function() {
       // 提交表单到数据库
       for (var i = 0; i < this.markersGroup.length; i++) {
         for (var j = 0; j < this.markersGroup[i].length; j++) {
@@ -188,6 +250,126 @@ export default {
         }
       }
       this.markersGroup = mountMap(this.map, this.travel_group_list);
+    },
+    editTravel: function(row) {
+      this.editRow = this.copy(row);
+      this.editIndex = this.indexOf(this.travel_group_list, row);
+      this.edit.addMode = false;
+      this.edit.modal = true;
+    },
+    del: function(row) {
+      for (var i = 0; i < this.travel_group_list.length; ++i) {
+        if (row == this.travel_group_list[i]) {
+          this.$backend.remove_travel_group(
+            {
+              user_id: this.$session.get("user_id"),
+              session_id: this.$session.id().replace("sess:", ""),
+              travel_group_id: row.travel_group_id
+            },
+            function(response) {
+              this.travel_group_list.splice(i, 1);
+              console.log(response);
+            },
+            function(response) {
+              alert(response.data.error_message);
+            }
+          );
+          break;
+        }
+      }
+    },
+    // ajax
+    add_travel_group: function(row) {
+      var vue = this;
+      var backend = this.$backend;
+      var session = this.$session;
+
+      backend.add_travel_group(
+        {
+          user_id: session.get("user_id"),
+          session_id: session.id().replace("sess:", ""),
+          travel_group_name: row.name,
+          travel_group_note: "hello",
+          travel_group_color: row.color.hex
+        },
+        function(response) {
+          row.travel.forEach(travel => {
+            backend.add_travel(
+              {
+                user_id: session.get("user_id"),
+                session_id: session.id().replace("sess:", ""),
+                city_id: travel.city_id,
+                date_start: travel.date_start,
+                date_end: travel.date_end,
+                visibility: travel.visibility,
+                travel_note: ""
+              },
+              function(response) {
+                travel.travel_id = response.data.travel_id;
+                console.log(response);
+              },
+              function(response) {
+                alert(response.data.error_message);
+              }
+            );
+          });
+          row.travel_group_id = response.data.travel_group_id;
+          vue.travel_group_list.push(row);
+          vue.$set(vue.travel_group_list, vue.editIndex, editRow);
+          vue.reMount();
+          console.log(response);
+        },
+        function(response) {
+          alert(response.data.error_message);
+        }
+      );
+    },
+
+    set_travel_group: function(editRow) {
+      var vue = this;
+      var backend = this.$backend;
+      var session = this.$session;
+
+      backend.set_travel_group_info(
+        {
+          user_id: session.get("user_id"),
+          session_id: session.id().replace("sess:", ""),
+          travel_group_id: editRow.travel_group_id,
+          travel_group_name: editRow.name,
+          travel_group_note: "hello",
+          travel_group_color: editRow.color.hex
+        },
+        function(response) {
+          editRow.travel.forEach(travel => {
+            backend.set_travel_info(
+              {
+                user_id: session.get("user_id"),
+                session_id: session.id().replace("sess:", ""),
+                travel_id: travel.travel_id,
+                city_id: travel.city_id,
+                date_start: travel.date_start,
+                date_end: travel.date_end,
+                visibility: travel.visibility,
+                travel_note: ""
+              },
+              function(response) {
+                console.log(response);
+              },
+              function(response) {
+                alert(response.data.error_message);
+              }
+            );
+          });
+
+          vue.travel_group_list[vue.editIndex] = editRow;
+          vue.$set(vue.travel_group_list, vue.editIndex, editRow);
+          vue.reMount();
+          console.log(response);
+        },
+        function(response) {
+          alert(response.data.error_message);
+        }
+      );
     }
   }
 };
@@ -200,13 +382,36 @@ export default {
   padding: 20px;
   /* width: 100px;
   height: 100px; */
-  z-index: 9999;
+  z-index: 999;
   background-color: rgba(255, 255, 255, 0.5);
 }
+.icon-expand {
+  position: relative;
+  margin-top: 10px;
+  margin-left: 10px;
+  font-size: 150%;
+  transition: transform 0.2s;
+}
 
-.button-text {
-  font-weight: 550;
-  padding: 8px;
-  color: rgba(255, 255, 255);
+.icon-expand:hover {
+  transform: scale(1.2);
+}
+.collapse {
+  display: none;
+  overflow: hidden;
+  background-color: #ffffff;
+}
+
+.expand {
+  display: block;
+  overflow: hidden;
+  background-color: #ffffff;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  z-index: 2;
 }
 </style>
