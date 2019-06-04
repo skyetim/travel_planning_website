1830cc970dc0ff990346915c354aeb16226cbcd5
Zhuo Liu <137310170@qq.com>
2019-06-04 13:22:19 +0800

    update recommend

--- src/backend/apps/api/modules/recommendation.py
+++ src/backend/apps/api/modules/recommendation.py
@@ -10,6 +10,7 @@ import apps.api.modules.user as mod_user
 from apps.api.modules.exceptions import *
 
 import math
+import numpy as np
 
 # 以下所有user如无意外均指User类的实例对象
 def get_time_delta_days(target_date):
@@ -26,33 +27,33 @@ def get_city_distance(city_id_1,city_id_2):
     Earth_Radius = 6378
 
     city_1 = mod_city.get_city_instance_by_id(city_id_1) #database对象
-    radLat1 = get_rad(city_1.latitutde)
-    radLon1 = get_rad(city_1.longtitude)
+    rad_Lat_1 = get_rad(city_1.latitutde)
+    rad_Lon_1 = get_rad(city_1.longtitude)
 
     city_2 = mod_city.get_city_instance_by_id(city_id_2) 
-    radLat2 = get_rad(city_2.latitutde)
-    radLon2 = get_rad(city_2.longtitude)
+    rad_Lat_2 = get_rad(city_2.latitutde)
+    rad_Lon_2 = get_rad(city_2.longtitude)
 
-    if radLat1 < 0: 
-    ryat1 = math.pi / 2 + math.abs(radLat1) #south  
-    if radLat1 > 0: 
-        radLat1 = math.pi / 2 - math.abs(radLat1) #north  
-    if radLon1 < 0:  
-        radLon1 = math.pi * 2 - math.abs(radLon1) #west  
-    if radLat2 < 0:  
-        radLat2 = math.pi / 2 + math.abs(radLat2) #south  
-    if radLat2 > 0:  
-        radLat2 = math.pi / 2 - math.abs(radLat2) #north  
-    if radLon2 < 0  
-        radLon2 = math.pi * 2 - math.abs(radLon2) #west     
+    if rad_Lat_1 < 0: 
+        rad_Lat_1 = math.pi / 2 + np.abs(rad_Lat_1) #south  
+    if rad_Lat_1 > 0: 
+        rad_Lat_1 = math.pi / 2 - np.abs(rad_Lat_1) #north  
+    if rad_Lon_1 < 0:  
+        rad_Lon_1 = math.pi * 2 - np.abs(rad_Lon_1) #west  
+    if rad_Lat_2 < 0:  
+        rad_Lat_2 = math.pi / 2 + np.abs(rad_Lat_2) #south  
+    if rad_Lat_2 > 0:  
+        rad_Lat_2 = math.pi / 2 - np.abs(rad_Lat_2) #north  
+    if rad_Lon_2 < 0:
+        rad_Lon_2 = math.pi * 2 - np.abs(rad_Lon_2) #west     
 
-    x1 = Earth_Radius * math.cos(radLon1) * math.sin(radLat1)
-    y1 = Earth_Radius * math.sin(radLon1) * math.sin(radLat1)
-    z1 = Earth_Radius * math.cos(radLat1)
+    x1 = Earth_Radius * math.cos(rad_Lon_1) * math.sin(rad_Lat_1)
+    y1 = Earth_Radius * math.sin(rad_Lon_1) * math.sin(rad_Lat_1)
+    z1 = Earth_Radius * math.cos(rad_Lat_1)
 
-    x2 = Earth_Radius * math.cos(radLon2) * math.sin(radLat2)
-    y2 = Earth_Radius * math.sin(radLon2) * math.sin(radLat2)
-    z2 = Earth_Radius * math.cos(radLat2)
+    x2 = Earth_Radius * math.cos(rad_Lon_2) * math.sin(rad_Lat_2)
+    y2 = Earth_Radius * math.sin(rad_Lon_2) * math.sin(rad_Lat_2)
+    z2 = Earth_Radius * math.cos(rad_Lat_2)
 
     d = math.sqrt((x1-x2)**2+(y1-y2)**2+(z1-z2)**2)
     theta = math.acos((Earth_Radius**2+Earth_Radius**2-d**2)/(2*Earth_Radius*Earth_Radius))
@@ -110,7 +111,7 @@ def recommend_city_list_by_travel(user, travel_id, amount=3):
     # 总数不超过amount
 
     user_id = user.get_user_id()
-    travel = mod_travel.Travel(user_id,travel_id) # travel 是Travel类实例对象
+    travel = mod_travel.Travel(user_id, travel_id) # travel 是Travel类实例对象
     travel_info = travel.get_travel_info()
     my_city = travel_info.get_city_id()
     friend_list = user.get_friend_list() # friend id list
@@ -123,11 +124,12 @@ def recommend_city_list_by_travel(user, travel_id, amount=3):
             if travel_list == []:
                 continue
             for travel_id in travel_list:
-                city = mod_Travel(user_id=user_id,travel_id=travel_id).get_travel_info().get_city_id()
-                other_travel_group_list.append(
-                    {"travel_id":travel_id, "city_distance":get_city_distance(my_city,city)}
-                )
-                
+                city = mod_travel.Travel(user_id=user_id, travel_id=travel_id).get_travel_info().get_city_id()
+                if city != my_city:
+                    other_travel_group_list.append(
+                        {"city_id":city, "city_distance":get_city_distance(my_city, city)}
+                    )
+
     other_travel_group_list.sort(key=lambda x: x["city_distance"])
 
     if len(other_travel_group_list) < amount:
@@ -136,7 +138,7 @@ def recommend_city_list_by_travel(user, travel_id, amount=3):
     recommend_list = []
 
     for i in range(amount):
-        recommend_list.append(other_travel_group_list[i][travel_id])            
+        recommend_list.append(other_travel_group_list[i]["city_id"])            
 
     return recommend_list
 
@@ -146,18 +148,124 @@ def recommend_city_list_by_travel_group(user, travel_group_id, amount=3):
     # 根据城市和该travel_group中已有的距离排序进行推荐
     # 不包括该travel_group中已有的city
     # 总数不超过amount
-    return
+
+    user_id = user.get_user_id()
+    travel_group = mod_travel.TravelGroup(user_id, travel_group_id)
+    travel_list = travel_group.get_travel_list()
+    my_city_list=[]
+    for travel in travel_list:
+        travel_info = travel.get_travel_info()
+        my_city = travel_info.get_city_id()
+        my_city_list.append(my_city)
+
+    friend_list = user.get_friend_list()
+
+    other_travel_group_list = []
+    for my_city in my_city_list:
+        for fr in friend_list:
+            for tg_id in user.get_other_travel_group_list(fr):
+                travel_group = mod_travel.TravelGroup(user_id=user_id, travel_group_id=tg_id)
+                travel_list = travel_group.get_travel_list()
+                if travel_list == []:
+                    continue
+                for travel_id in travel_list:
+                    city = mod_travel.Travel(user_id=user_id, travel_id=travel_id).get_travel_info().get_city_id()
+                    if city not in my_city_list:
+                        other_travel_group_list.append(
+                            {"city_id":city, "city_distance":get_city_distance(my_city, city)}
+                        )
+
+    other_travel_group_list.sort(key=lambda x: x["city_distance"])
+
+    if len(other_travel_group_list) < amount:
+        amount = len(other_travel_group_list)
+
+    recommend_list = []
+
+    for i in range(amount):
+        recommend_list.append(other_travel_group_list[i]["city_id"])   
+
+    return recommend_list
 
 
 def recommend_travel_list_by_travel(user, travel_id, amount=5):
     # 遍历好友的旅行记录
     # 根据城市和该travel的距离、时间间隔排序进行推荐
     # 总数不超过amount
-    return
+
+    user_id = user.get_user_id()
+    travel = mod_travel.Travel(user_id, travel_id) # travel 是Travel类实例对象
+    travel_info = travel.get_travel_info()
+    my_city = travel_info.get_city_id()
+    friend_list = user.get_friend_list() # friend id list
+
+    other_travel_group_list = []
+    for fr in friend_list:
+        for tg_id in user.get_other_travel_group_list(fr):
+            travel_group = mod_travel.TravelGroup(user_id=user_id, travel_group_id=tg_id)
+            travel_list = travel_group.get_travel_list()
+            if travel_list == []:
+                continue
+            rep_time = mod_travel.Travel(user_id=user_id, travel_id=travel_list[0]).get_travel_info().get_date_start()
+            for travel_id in travel_list:
+                city = mod_travel.Travel(user_id=user_id, travel_id=travel_id).get_travel_info().get_city_id()
+                if city != my_city:
+                    other_travel_group_list.append(
+                        {"travel_id":travel_id, "time_delta_days": get_time_delta_days(rep_time), "city_distance":get_city_distance(my_city, city)}
+                    ) 
+    other_travel_group_list.sort(key=lambda x: np.sqrt((int(x["city_distance"] / 50))**2 + (int(x["time_delta_days"] / 1))**2))
+
+    if len(other_travel_group_list) < amount:
+        amount = len(other_travel_group_list)
+
+    recommend_list = []
+
+    for i in range(amount):
+        recommend_list.append(other_travel_group_list[i][travel_id])   
+
+    return recommend_list
 
 
 def recommend_travel_list_by_travel_group(user, travel_group_id, amount=5):
     # 遍历好友的旅行记录
     # 根据城市和该travel_group中城市的距离、时间间隔排序进行推荐
     # 总数不超过amount
-    return
+    user_id = user.get_user_id()
+    travel_group = mod_travel.TravelGroup(user_id, travel_group_id)
+    travel_list = travel_group.get_travel_list()
+    my_city_list=[]
+    for travel in travel_list:
+        travel_info = travel.get_travel_info()
+        my_city = travel_info.get_city_id()
+        my_city_list.append(my_city)
+
+    friend_list = user.get_friend_list()
+
+    other_travel_group_list = []
+    for my_city in my_city_list:
+        for fr in friend_list:
+            for tg_id in user.get_other_travel_group_list(fr):
+                travel_group = mod_travel.TravelGroup(user_id=user_id, travel_group_id=tg_id)
+                travel_list = travel_group.get_travel_list()
+                if travel_list == []:
+                    continue
+                rep_time = mod_travel.Travel(user_id=user_id, travel_id=travel_list[0]).get_travel_info().get_date_start()
+                for travel_id in travel_list:                   
+                    city = mod_travel.Travel(user_id=user_id, travel_id=travel_id).get_travel_info().get_city_id()
+                    if city not in my_city_list:
+                        other_travel_group_list.append(
+                            {"travel_id":travel_id, "time_delta_days": get_time_delta_days(rep_time),"city_distance":get_city_distance(my_city, city)}
+                        )
+
+    other_travel_group_list.sort(key=lambda x: np.sqrt((int(x["city_distance"] / 50))**2 + (int(x["time_delta_days"] / 1))**2))
+
+    if len(other_travel_group_list) < amount:
+        amount = len(other_travel_group_list)
+
+    recommend_list = []
+
+    for i in range(amount):
+        recommend_list.append(other_travel_group_list[i][travel_id])   
+
+    return recommend_list
+
