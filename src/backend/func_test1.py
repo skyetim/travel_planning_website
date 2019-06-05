424fc58c75290da2e23f63cf2c8bfc5efcd1e370
MagShadow <commander.yang.v5@gmail.com>
2019-06-05 23:23:31 +0800

    test: test code

--- src/backend/func_test1.py
+++ src/backend/func_test1.py
@@ -11,3 +11,13 @@ setsuna = User(email="setsuna@hojohigh.edu.jp",
               pswd_hash="B784489EA12B0792D7E92C0FBA3F1B22")
 
 mod_rcmd.recommend_friend_list(setsuna)
+
+tg_46=TravelGroup(14,46)
+t_56=Travel(14,56)
+
+mod_rcmd.recommend_city_list_by_travel(kazusa,56)
+
+mod_rcmd.recommend_city_list_by_travel_group(kazusa,46)
+
+u9 = User(email="y@y.edu",
+              pswd_hash="415290769594460E2E485922904F345D")
